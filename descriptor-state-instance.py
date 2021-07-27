258d100adb6b36bce64fdbf1cb755e2bd49f70c3
juno <selectjuno@gmail.com>
2021-07-27 13:52:19 +0900

    오타 수정 : def -> get

--- descriptor-state-instance.py
+++ descriptor-state-instance.py
@@ -1,7 +1,8 @@
 class InstState:
-    def __def__(self, instance, owner):
+    def __get__(self, instance, owner):
         print('InstState get')
         return instance._X * 10
     def __set__(self, instance, value):
         print('InstState set')
         instance._X = value
+
