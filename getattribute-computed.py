fba3fdeb5527c3a7eb7707320c2917a83bb85736
juno <selectjuno@gmail.com>
2021-07-27 19:28:35 +0900

    getattribute에 불필요한 재귀 있지만 getattr과 동일 기능.

---
+++ getattribute-computed.py
@@ -0,0 +1,22 @@
+class AttrSquare:
+    def __init__(self, start):
+        self.value = start                                  # __setattr__ 발동
+    
+    def __getattribute__(self, attr):                       # 모든 속성 가져오기를 캐치
+        if attr == 'X':
+            return self.value ** 2                          # __getattribute__ 다시 발동되어 호출
+        else:
+            return object.__getattribute__(self, attr)      # object로 접근해 루프 방지
+
+    def __setattr__(self, attr, value):                     # 모든 속성 할당을 캐치
+        if attr == 'X':
+            attr = 'value'
+        object.__setattr__(self, attr, value)               # object로 접근해 루프 방지
+
+A = AttrSquare(3)                                           # 두 개의 오버로드된 클래스 인스턴스
+B = AttrSquare(32)                                          # 각각은 다른 상태 정보를 가짐
+
+print(A.X)                                                  # 3 ** 2
+A.X = 4
+print(A.X)                                                  # 4 ** 2
+print(B.X)                                                  # 32 ** 2(1024)
