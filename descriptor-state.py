ff533e229c80fef3d007548d0b67e8a3187b6b56
juno <selectjuno@gmail.com>
2021-07-27 13:07:54 +0900

    상태정보 사용하는 디스크립터를 클래스 변수로 사용하는 클래스 정의

--- descriptor-state.py
+++ descriptor-state.py
@@ -9,3 +9,10 @@ class DescState:
     def __set__(self, instance, value):     # 속성을 할당할 때 실행됨
         print('DescState set')
         self.value = value
+
+# 클라이언트 클래스
+class CalcAttrs:
+    X = DescState(2)        # 디스크립터 클래스 속성(=변수)
+    Y = 3                   # 클래스 속성(=변수)
+    def __init__(self):
+        self.Z = 4          # 인스턴스 속성(=변수)
